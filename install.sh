f1be6a75439e62a51db28d6f15602df4ad3f9f2f
arthurvrt-nw <arthur.verot@nw-groupe.com>
2025-06-09 12:01:58 +0200

    feat: use curl instead of wget

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ if ! command -v zsh &> /dev/null; then
   echo "Zsh not found. Please install zsh first."
   exit 1
 fi
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O - | zsh || true
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh || true
 
 # Install useful zsh plugins
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
