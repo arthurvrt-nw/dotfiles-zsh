36c9b46871af09d377b5578a280a928a544ea882
arthurvrt-nw <arthur.verot@nw-groupe.com>
2025-06-09 11:20:25 +0200

    feat: .zshrc and install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# powerline fonts for zsh agnoster theme
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh & plugins
+# https://github.com/ohmyzsh/ohmyzsh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O - | zsh || true
+
+# Install useful zsh plugins
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+
+# Copy your custom .zshrc, and force the robbyrussell theme
+cp ./.zshrc ~
+sed -i '/^ZSH_THEME/c\ZSH_THEME="robbyrussell"' ~/.zshrc
